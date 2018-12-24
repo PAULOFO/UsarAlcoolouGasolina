3fdecbb00d832a219a9cbe25ff1a70a7153b3e67
PAULOFO <paulofo.forever@gmail.com>
2018-12-24 13:34:00 -0200

    Exibindo conteúdos

--- app/src/main/java/com/whatsapp/magazine/paulooliveira/usaralcoolougasolina/activity/MainActivity.java
+++ app/src/main/java/com/whatsapp/magazine/paulooliveira/usaralcoolougasolina/activity/MainActivity.java
@@ -1,10 +1,8 @@
-package com.whatsapp.magazine.paulooliveira.usaralcoolougasolina;
+package com.whatsapp.magazine.paulooliveira.usaralcoolougasolina.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,8 +10,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.whatsapp.magazine.paulooliveira.usaralcoolougasolina.R;
+import com.whatsapp.magazine.paulooliveira.usaralcoolougasolina.fragment.PrincipalFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -26,8 +28,14 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Carrega tela principal
+        PrincipalFragment principalFragment = new PrincipalFragment();
+        FragmentTransaction fragment = getSupportFragmentManager().beginTransaction();
+        //fragment.replace(R.id., principalFragment );
+        fragment.commit();
 
         editPrecoAlcool = findViewById(R.id.editPrecoAlcool);
         editPrecoGasolina = findViewById(R.id.editPrecoGasolina);
@@ -42,20 +50,20 @@ public class MainActivity extends AppCompatActivity
             }
         });*/
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -92,7 +100,12 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
+            PrincipalFragment principalFragment = new PrincipalFragment();
+            FragmentTransaction fragment = getSupportFragmentManager().beginTransaction();
+            //fragment.replace(R.id.activity_main, principalFragment );
+            fragment.commit();
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -105,7 +118,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
---
+++ app/src/main/java/com/whatsapp/magazine/paulooliveira/usaralcoolougasolina/fragment/AlcoolFragment.java
@@ -0,0 +1,30 @@
+package com.whatsapp.magazine.paulooliveira.usaralcoolougasolina.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.whatsapp.magazine.paulooliveira.usaralcoolougasolina.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AlcoolFragment extends Fragment {
+
+
+    public AlcoolFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alcool, container, false);
+    }
+
+}
